294d7d3cc4aa4c943be88863c686ce3aebc5e1e2
AnnieChris <annieesther3@gmail.com>
2022-03-04 12:25:29 -0600

    Day3 assignmet styles updated

--- assignments/javascript/luckynumber.js
+++ assignments/javascript/luckynumber.js
@@ -10,13 +10,15 @@ function numcheck()
     if(lucky_number == 3)
     {
         
-        document.getElementById('display_result').innerHTML = "Great!! You Guessed my lucky number at attempt "+click_count;
+        document.getElementById("result").innerHTML = "Great!! You Guessed my lucky number at attempt "+click_count;
         click_count = 0;
+        document.getElementById("result").className = "display_result";
         /* document.getElementById('display_result').style.backgroundColor = "blue"; */
     }
     else
     {
         
-        document.getElementById('display_result').innerHTML = "Not Really. Give it another try";
+        document.getElementById('result').innerHTML = "Not Really. Give it another try";
+        document.getElementById("result").className = "display_result";
     }
 }
