7def424406a16b6441df097c13ac08c39fbf22f6
AnnieChris <annieesther3@gmail.com>
2022-03-16 00:44:59 -0500

    React Project added to git

---
+++ react-project/react-code/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Welcome to My Page</h1>
+     
+    </div>
+  );
+}
+
+export default App;
+
---
+++ react-project/react-code/src/component/NewFunctionalComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewFunctionalComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default NewFunctionalComponent;
